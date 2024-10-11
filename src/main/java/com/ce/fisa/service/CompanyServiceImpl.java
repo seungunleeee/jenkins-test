2851ee05bef323977d04e6de8d8b13c92c34f9c6
seungunleeee <90971532+seungunleeee@users.noreply.github.com>
2024-10-11 12:26:41 +0900

    Update CompanyServiceImpl.java v3

--- src/main/java/com/ce/fisa/service/CompanyServiceImpl.java
+++ src/main/java/com/ce/fisa/service/CompanyServiceImpl.java
@@ -57,7 +57,7 @@ public class CompanyServiceImpl implements CompanyService{
 
 	@Override
 	public List<DeptDTO> getDeptAll() {
-	    log.info("*** 모든 부서 정보 요청 V2 !!! ***");
+	    log.info("*** 모든 부서 정보 요청 V3 !!! ***");
 	    List<Dept> deptEntities = deptDao.findAll();
 	    System.out.println(deptEntities);
 	     // 유틸리티 메서드로 리스트 매핑 처리
